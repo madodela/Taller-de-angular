32607d313e9e11c0f068e37e1095149b83ce36db
Dolores Delgado <dolores.delgado@ge.com>
2016-08-08 12:00:09 -0500

    Practice 4: Creating a custom filter

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -51,4 +51,31 @@ angular.module('practice1App')
     ];
   
     $scope.filteredArray = $filter('filter')($scope.filterArray,{name:'lu'});
+  })
+  .filter('bytesFormat', function () {
+    return function (size) {
+      if (isNaN(size))
+        size = 0;
+      if (size < 1024)
+        return size + ' Bytes';
+
+      size /= 1024;
+
+      if (size < 1024)
+        return size.toFixed(2) + ' Kb';
+
+      size /= 1024;
+
+      if (size < 1024)
+        return size.toFixed(2) + ' Mb';
+
+      size /= 1024;
+
+      if (size < 1024)
+        return size.toFixed(2) + ' Gb';
+
+      size /= 1024;
+
+      return size.toFixed(2) + ' Tb';
+    };
   });
